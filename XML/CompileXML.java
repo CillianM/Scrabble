83bf78d65c7e7417b059b93e1da77fea043cd9bd
unixbash <filip500filip@gmail.com>
2016-11-22 23:40:53 +0000

    Fully featured version of CompileXML
    Containing, connect, updatePlayer and makeMove methods.

--- XML/CompileXML.java
+++ XML/CompileXML.java
@@ -12,7 +12,21 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import java.util.Arrays;
+
 public class CompileXML {
+
+	public static String createFile(String file) {
+		//Create XML file
+		String filename = file;
+		String workingDirectory = System.getProperty("user.dir");
+
+		String absoluteFilePath = "";
+		absoluteFilePath = workingDirectory + File.separator + filename;
+		System.out.println("File saved at: " + absoluteFilePath);
+
+		return absoluteFilePath; 
+	}
 
 	public static void connect(String names [], String curentPlayer,char newRack[]) {
 		try {
@@ -59,21 +73,110 @@ public class CompileXML {
 			Transformer transformer = transformerFactory.newTransformer();
 			DOMSource source = new DOMSource(doc);
 
-			//Create XML file
-			String filename = "connect.xml";
-			String workingDirectory = System.getProperty("user.dir");
-
-			String absoluteFilePath = "";
-
-			absoluteFilePath = workingDirectory + File.separator + filename;
-
-			System.out.println("Final filepath : " + absoluteFilePath);
-
-			StreamResult result = new StreamResult(new File(absoluteFilePath));
-
+			
+			StreamResult result = new StreamResult(new File(createFile("connect.xml")));
 			transformer.transform(source, result);
 
-			System.out.println("File saved!");
+		  	} 
+	 	catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+	  	} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+	  	}
+	}
+
+	public static void makeMove(CellSetter [] move) {
+		try {
+			DocumentBuilderFactory docFact = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuild = docFact.newDocumentBuilder();
+
+			Document doc = docBuild.newDocument();
+			Element rootElement = doc.createElement("move");
+			doc.appendChild(rootElement);
+
+			//Create the User entries to be shared with
+			for (int i=0;i < move.length; i++) {
+				String tileNo = "tile"+(i+1);
+				//Tile
+				Element tile = doc.createElement(tileNo);
+				rootElement.appendChild(tile);
+				//TileVal
+				Element tileVal = doc.createElement("tileVal");
+				String tileChar = ""+move[i].character;
+				tileVal.appendChild(doc.createTextNode(tileChar));
+				tile.appendChild(tileVal);
+				//Points
+				Element pos = doc.createElement("pos");
+				String posInt = ""+move[i].postion.getX()+","+move[i].postion.getX();
+				pos.appendChild(doc.createTextNode(posInt));
+				tile.appendChild(pos);
+				//IsSpace
+				Element isSpace = doc.createElement("isSpace");
+				String space = "";
+				if(move[i].isSpace)	{
+					space = "true";
+				}
+				else {
+					space = "false";
+				}
+				isSpace.appendChild(doc.createTextNode(space));
+				tile.appendChild(isSpace);
+			}
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+
+			StreamResult result = new StreamResult(new File(createFile("move.xml")));
+			transformer.transform(source, result);
+
+		  	} 
+	 	catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+	  	} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+	  	}
+	}
+
+	public static void updatePlayer(char [] rack, int [] allScores, int currentPlayer) {
+		try {
+			DocumentBuilderFactory docFact = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuild = docFact.newDocumentBuilder();
+
+			Document doc = docBuild.newDocument();
+			Element rootElement = doc.createElement("update");
+			doc.appendChild(rootElement);
+
+			Element tiles = doc.createElement("tiles");
+			rootElement.appendChild(tiles);
+
+			//Create the User entries to be shared with
+			for (int i=0;i < rack.length; i++) {
+				String tileNo = "tile"+(i+1);
+				//Tile
+				Element tile = doc.createElement(tileNo);
+				String tileChar = ""+rack[i];
+				tile.appendChild(doc.createTextNode(tileChar));
+				tiles.appendChild(tile);
+			}
+			//Scores
+			Element scores = doc.createElement("scores");
+			String pointsInt = Arrays.toString(allScores);
+			scores.appendChild(doc.createTextNode(pointsInt));
+			rootElement.appendChild(scores);
+
+			//Curent move
+			Element currentMove = doc.createElement("currentMove");
+			String current = ""+currentPlayer;
+			currentMove.appendChild(doc.createTextNode(current));
+			rootElement.appendChild(currentMove);
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+
+			StreamResult result = new StreamResult(new File(createFile("updatePlayer.xml")));
+			transformer.transform(source, result);
 
 		  	} 
 	 	catch (ParserConfigurationException pce) {
@@ -84,9 +187,15 @@ public class CompileXML {
 	}
 
 	public static void main(String args[]) {
-		CellSetter send = new CellSetter();
+		CellSetter send1 = new CellSetter('a',1,2, false);
+		CellSetter send2 = new CellSetter('b',3,4, false);
+		CellSetter send3 = new CellSetter(' ',5,6, true);
+		CellSetter send[]={send1,send2,send3};
+		int scores[] = {56,68,11,44};
 		char newRack[] = {'a','a','b','a','b','a','b'};
 		String arr[] = {"Filip", "Cillian", "Shaun"};
-		connect(arr, "Filip", newRack);
+		//connect(arr, "Filip", newRack);
+		//makeMove(send);
+		updatePlayer(newRack, scores, 2);
 	}
 }
